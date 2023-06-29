48a6a796dbe41e6978c21d793e3b8d614e038658
leejr <jnkinyanjui35@gmail.com>
2023-06-29 16:40:51 +0300

    First Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+
+number = int(input("Enter a number to be checked"))
+
+if number%2 == 0:
+    print("The number is even")
+elif number%2 == 1:
+    print("The number is an odd number")
+    if  number/1 == number and number/number == 1 and number%3 != 0 :
+        print("The number is a prime number")
+else:
+    print("The number seems not to be existing!!!")
+
+
